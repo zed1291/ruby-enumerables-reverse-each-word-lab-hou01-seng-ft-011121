281ad090cef2f1917804f02caaf5aa870165aef5
zed <zed@kolk.me>
2020-12-29 23:46:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
 def reverse_each_word(phrase)
   def reverse_each_word(phrase)
-    greeting = phrase.split(' ')
-    results = ''
-    greeting.collect do |word|
-      results += "#{word.reverse} "
-    end
-    results.delete_suffix!(' ')
     # greeting = phrase.split(' ')
     # results = ''
-    # greeting.each { |word| results += "#{word.reverse} " }
+    # greeting.collect do |word|
+    #   results += "#{word.reverse} "
+    # end
     # results.delete_suffix!(' ')
+    greeting = phrase.split(' ')
+    results = ''
+    greeting.each { |word| results += "#{word.reverse} " }
+    results.delete_suffix!(' ')
   end
 end
