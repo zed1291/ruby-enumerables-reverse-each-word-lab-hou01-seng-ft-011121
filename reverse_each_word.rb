b6267dc8d024d0a110c11cf644c39851891f17a4
zed <zed@kolk.me>
2020-12-29 23:43:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(phrase)
     greeting = phrase.split(' ')
     results = ''
     greeting.collect do |word|
-      results += word.reverse
+      results += "#{word.reverse} "
     end
     results.delete_suffix!(' ')
     results
