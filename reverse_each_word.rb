0616d521c14868b857555756a0486190125a8e4a
zed <zed@kolk.me>
2020-12-29 23:51:32 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(phrase)
+    greeting = phrase.split(' ')
+    results = ''
+    greeting.collect do |word|
+      results += "#{word.reverse} "
+    end
+    results.delete_suffix!(' ')
+
+    # greeting = phrase.split(' ')
+    # results = ''
+    # greeting.each { |word| results += "#{word.reverse} " }
+    # results.delete_suffix!(' ')
+end
